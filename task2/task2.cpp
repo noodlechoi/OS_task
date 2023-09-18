bfdfd6e753ab9b8e8f297740bb0f762e079031e8
minachoi <choime321@gmail.com>
2023-09-19 00:17:39 +0900

    프로세스가 끝나기 전까지 기다리는 코드

--- task2/task2.cpp
+++ task2/task2.cpp
@@ -1,42 +1,61 @@
-﻿#include<stdio.h>
-#include<windows.h>
-#include<tchar.h>
+﻿#include <stdio.h>
+#include <windows.h>
 
-int main(int argc, TCHAR* argv[])
+int main(int argc, char* argv[])
 {
-    // 구조체 초기화
-    STARTUPINFO si = { 0, };
-    // 이전 프로세스 정보를 저장하는 구조체
+    DWORD dwExitCode = 0, dwRet = 0;
+    HANDLE hProc[2];
+    STARTUPINFO si;
     PROCESS_INFORMATION pi;
 
+    ZeroMemory(&si, sizeof(si));
     si.cb = sizeof(si);
-    si.dwFlags = STARTF_USEPOSITION | STARTF_USESIZE;
-    si.dwX = 100;
-    si.dwY = 100;
-    si.dwXSize = 300;
-    si.dwYSize = 300;
+    ZeroMemory(&pi, sizeof(pi));
 
-    TCHAR command[] = _T("notepad 1");
-
-    BOOL state;
-
-    state = CreateProcess(
-        NULL,
-        command,
-        NULL, NULL,
-        TRUE,
-        CREATE_NEW_CONSOLE,
-        NULL, NULL,
-        &si,
-        &pi
-    );
-
-    if (state != 0) {
-        _fputts(_T("Creation OK! \n"), stdout);
-    }
-    else {
-        _fputts(_T("Creation Fail! \n"), stdout);
+    //  Child Process 실행
+    TCHAR command[] = TEXT("notepad 1");
+    if (!CreateProcess(NULL, command, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
+    {
+        printf("fail to execute process \n");
+        return-1;
     }
 
+    if (GetExitCodeProcess(pi.hProcess, &dwExitCode))
+    {
+        //   process가 살아 있는 경우, dwExitCode의 값은 STILL_ACTIVE 입니다.
+
+        if (dwExitCode == STILL_ACTIVE)
+            printf("process is alive\n");
+        else
+            printf("exit status(code) : %d\n", dwExitCode);
+    }
+    else
+    {
+        printf("errcode : %d\n", GetLastError());
+    }
+
+    hProc[0] = pi.hProcess;
+    dwRet = WaitForMultipleObjects(1, hProc, FALSE, INFINITE);
+
+    //  WaitForMultipleObjects()의 반환값은 시그널된 객체(Process, Thread)의 ID 값이며,
+    // 다음과 같은 형태로 어느 객체가 시그널 되어 있는지 확인이 가능합니다.
+
+    if (dwRet == WAIT_OBJECT_0 + 0)
+    {
+
+        // 자~~ 이제 Child Process가 종료시에 반환한 값을 확인 !!
+
+        if (GetExitCodeProcess(*(hProc + 0), &dwExitCode))
+        {
+            printf("exit status(code) : %d\n", dwExitCode);
+        }
+        else
+        {
+            printf("errcode : %d\n", GetLastError());
+        }
+    }
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
     return 0;
 }
